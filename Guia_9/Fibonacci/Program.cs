5e6b4748d78da865bd0988fb78625d4ae3911b39
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-06-01 04:18:34 -0300

    Ej1

---
+++ Guia_9/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contTerminos = 1;
+            int valorAct = 0;
+            int valorAnt = 1;
+            int valorSiguiente = 0;
+            Console.WriteLine("Ingrese cantidad de terminos");
+            int terminos = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= terminos; i++)
+            {
+                if (contTerminos<=1)
+                {
+                    valorAct = 1;
+                    valorAnt = 0;
+                    Console.WriteLine(valorAnt);
+                    Console.WriteLine(valorAct);
+                }
+                else
+                {
+                    valorSiguiente = valorAct + valorAnt;
+                    valorAnt = valorAct;
+                    valorAct = valorSiguiente;
+                    Console.WriteLine(valorSiguiente);
+                }
+                contTerminos++;
+            }
+        }
+    }
+}
---
+++ Guia_9/Fibonacci_Recursivo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Fibonacci_Recursivo
+{
+    internal class Program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese cantidad de terminos");
+            int terminos = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < terminos; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+    }
+}
