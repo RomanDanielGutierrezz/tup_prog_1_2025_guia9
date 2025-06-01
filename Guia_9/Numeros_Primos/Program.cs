401cb1a251ae11acc31f0f2f8855c8caf2ae9ccc
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-06-01 04:43:51 -0300

    Primos

---
+++ Guia_9/Numeros_Primos/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Numeros_Primos
+{
+    internal class Program
+    {
+        static bool Primo(int n, int divisor = 2)
+        {
+            if (n % divisor == 0)
+                return false;
+            if (divisor * divisor > n)
+                return true;
+            if (n < 2) 
+                return false;
+            return Primo(n, divisor + 1);
+        }
+        static void Main(string[] args)
+        {
+            int n = 0; int m = 0;
+            Console.WriteLine("Indique el numeor inical");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indique el numero final");
+            m = Convert.ToInt32(Console.ReadLine());
+            for (int i = n; i <= m; i++)
+            {
+                if (Primo(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
